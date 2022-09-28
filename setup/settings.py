dcf8831c38d721af3b3147116727e34d5ea62441
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-09-28 16:14:56 -0300

    dbsqlite

--- setup/settings.py
+++ setup/settings.py
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'imobiliaria',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '5432'
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'imobiliaria',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',
+#        'HOST': '127.0.0.1',
+#        'DATABASE_PORT': '5432'
 #    }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
