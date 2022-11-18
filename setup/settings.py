7525be7f0d6b44c7ac642503b0db4f10ccf5f161
lucasoottoni <104531657+lucasoottoni@users.noreply.github.com>
2022-11-18 11:11:01 -0300

    set-base

--- setup/settings.py
+++ setup/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'base-dados/db.sqlite3'),
     }
 }
 
